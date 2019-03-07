acce724b1f784d13ef2306f20b4ddfd346f4fcb4
bdqsky <bdqsky@163.com>
2019-03-08 00:14:03 +0800

    bug fix

--- rpc-core/src/main/java/cn/bdqfork/rpc/netty/consumer/ClientContextHandler.java
+++ rpc-core/src/main/java/cn/bdqfork/rpc/netty/consumer/ClientContextHandler.java
@@ -3,6 +3,7 @@ package cn.bdqfork.rpc.netty.consumer;
 import cn.bdqfork.rpc.netty.consumer.context.RpcContextManager;
 import cn.bdqfork.rpc.protocol.NettyChannel;
 import cn.bdqfork.rpc.protocol.RpcResponse;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -10,6 +11,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * @author bdq
  * @date 2019-02-20
  */
+@ChannelHandler.Sharable
 public class ClientContextHandler extends ChannelInboundHandlerAdapter {
 
     @Override
--- rpc-core/src/main/java/cn/bdqfork/rpc/netty/provider/InvokerHandler.java
+++ rpc-core/src/main/java/cn/bdqfork/rpc/netty/provider/InvokerHandler.java
@@ -3,6 +3,7 @@ package cn.bdqfork.rpc.netty.provider;
 import cn.bdqfork.rpc.protocol.invoker.Invocation;
 import cn.bdqfork.rpc.protocol.invoker.Invoker;
 import cn.bdqfork.rpc.protocol.RpcResponse;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -10,8 +11,9 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * @author bdq
  * @date 2019-02-20
  */
+@ChannelHandler.Sharable
 public class InvokerHandler extends ChannelInboundHandlerAdapter {
-    private Invoker<RpcResponse> invoker;
+    private final Invoker<RpcResponse> invoker;
 
     public InvokerHandler(Invoker<RpcResponse> invoker) {
         this.invoker = invoker;
